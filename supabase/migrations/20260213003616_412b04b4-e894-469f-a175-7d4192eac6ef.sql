824039d82798df64132b4e57ba98c437b05da30e
obgomes <87842009+obgomes@users.noreply.github.com>
2026-02-13 02:25:48 +0000

    Deploying to stable-website from @ obgomes/vitasigma@62782c0c1d1a708a12fa87ac38897ea3953210e4 🚀

--- supabase/migrations/20260213003616_412b04b4-e894-469f-a175-7d4192eac6ef.sql
+++
@@ -1,7 +0,0 @@
-CREATE POLICY "Allow anonymous select on chat_leads"
-  ON public.chat_leads FOR SELECT
-  TO anon USING (true);
-
-CREATE POLICY "Allow anonymous select on chat_messages"
-  ON public.chat_messages FOR SELECT
-  TO anon USING (true);
